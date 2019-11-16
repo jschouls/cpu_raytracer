15600ece4c1618cec14563125485947acf2e69ea
jschouls <Joris.schouls@gmail.com>
2019-11-16 14:37:21 +0100

    Basic sdl2 window with pixel drawing.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,52 @@
 #![warn(clippy::all)]
 
-fn main() {
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::pixels;
+use sdl2::rect::Point;
+
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+
+fn main() -> Result<(), String> {
     // test
     println!("Hello, world!");
+
+    let sdl_context = sdl2::init()?;
+    let video_subsys = sdl_context.video()?;
+    let window = video_subsys
+        .window("CPU Raytracer", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut events = sdl_context.event_pump()?;
+
+    'main: loop {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'main,
+
+                Event::MouseButtonDown { x, y, .. } => {
+                    canvas.set_draw_color(pixels::Color::RGB(255, 255, 255));
+                    let p = Point::new(x, y);
+                    canvas.draw_point(p)?;
+                    println!("mouse btn down at ({},{})", x, y);
+                    canvas.present();
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }

0d8ea962d654d15724f5da45a50fcff5bcc89aa1
jschouls <Joris.schouls@gmail.com>
2020-04-05 18:52:35 +0200

    Added ray generation function

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,5 @@
 use super::material::Material;
-use super::vector::Vec3;
+use super::Vec3;
 use std::rc::Rc;
 
 pub enum IntersectData {
@@ -15,4 +15,23 @@ pub struct Ray {
     pub direction: Vec3,
     pub origin: Vec3,
     pub travel_distance: f32,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Ray {
+            is_intersected: IntersectData::None,
+            direction: direction,
+            origin: origin,
+            travel_distance: std::f32::MAX,
+        }
+    }
+
+    //
+    pub fn set(&mut self, origin: Vec3, direction: Vec3) {
+        self.is_intersected = IntersectData::None;
+        self.travel_distance = std::f32::MAX;
+        self.direction = direction;
+        self.origin = origin;
+    }
 }
